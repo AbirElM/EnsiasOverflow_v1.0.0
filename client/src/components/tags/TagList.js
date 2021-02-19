675039abb9cd02352eed54d0b61570ef2c77d798
AbirElM <abirelmokhtari@gmail.com>
2021-02-19 14:28:57 +0100

    Minor fixes

--- client/src/components/tags/TagList.js
+++ client/src/components/tags/TagList.js
@@ -1,47 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import './tags.css'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./tags.css";
+import { Link } from "react-router-dom";
+
 function TagList() {
-    const [tags,setTags]= useState();
-    const [filtertags,setFiltrtags]= useState();
-       
-    useEffect(async ()=>{
-      try{
-         await axios.get('/tags/all').then(res=>setTags(res.data)).catch(err=> console.log(err))
-       
-        
-      }catch(err){
-        console.log(err)
-      }
-      try {
-      } catch (error) {
-        console.log("catched err" + error);
-      }
+  const [tags, setTags] = useState();
+  const [filtertags, setFiltrtags] = useState();
 
-    },[])
-
-
-
-
-
-    if(tags){return (
-        <div>
-            <h1>List of tags</h1>
-            <div className="tags">
-            {tags.map((tag)=>(
-                <div className="tag">
-                <Link to={`/tags/all/tag/${tag}`} className='tg'>{tag}</Link>
-                </div>
-            ))}
-            </div>
-           
-        </div>
-    )}else{
-        return(
-            <div>Hello</div>
-        )
+  useEffect(async () => {
+    try {
+      await axios
+        .get("/tags/all")
+        .then((res) => setTags(res.data))
+        .catch((err) => console.log(err));
+    } catch (err) {
+      console.log(err);
     }
+    try {
+    } catch (error) {
+      console.log("catched err" + error);
+    }
+  }, []);
+
+  if (tags) {
+    return (
+      <div>
+        <h1>List of tags</h1>
+        <div className="tags">
+          {tags.map((tag) => (
+            <div className="tag">
+              <Link to={`/tags/all/tag/${tag}`} className="tg">
+                {tag}
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return <div>Hello</div>;
+  }
 }
 
-export default TagList
+export default TagList;
